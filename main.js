97f3882c3fcb31b4475171204d67f5fbbf533e9f
HamSudo <192023912+HamSudo@users.noreply.github.com>
2025-08-06 11:20:20 +0400

    Add classes to inner squares and outer containers in Javascript, and style using Flexbox to make the divs appear as a grid

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ for (let i = 0; i < 16; i++) {
     const outer = document.createElement("div");
     for (let j = 0; j < 16; j++){
         const inner = document.createElement('div');
+        inner.setAttribute("class", "inner");
         outer.appendChild(inner);
     }
+    outer.setAttribute("class", "outer");
     container.appendChild(outer);
 }
