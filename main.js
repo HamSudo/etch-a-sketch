b61d1678c185aa314725756e47da2bf921866638
HamSudo <192023912+HamSudo@users.noreply.github.com>
2025-08-06 10:11:49 +0400

    Create 16×16 grid of square divs using Javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+    const outer = document.createElement("div");
+    for (let j = 0; j < 16; j++){
+        const inner = document.createElement('div');
+        outer.appendChild(inner);
+    }
+    container.appendChild(outer);
+}
