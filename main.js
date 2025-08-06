835cd1caf26252656d2a294c05eac0b29ca44cf7
HamSudo <192023912+HamSudo@users.noreply.github.com>
2025-08-06 12:56:16 +0400

    Add a button for the user to enter the grid dimensions, and replace outer and inner div containers with div square boxes as direct children of the main container

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
-const container = document.querySelector("#container");
+function askForDimensions() {
+    const min = 1;
+    const max = 100;
 
-for (let i = 0; i < 16; i++) {
-    const outer = document.createElement("div");
-    for (let j = 0; j < 16; j++){
-        const inner = document.createElement('div');
-        inner.setAttribute("class", "inner");
-        inner.addEventListener("mouseover", (e) => e.target.classList.add('hover'));
-        outer.appendChild(inner);
-    }
-    outer.setAttribute("class", "outer");
-    container.appendChild(outer);
+    let dim;
+    do {
+        dim = prompt((`Enter the dimension for the new squared grid between ${min} and ${max}:`));
+        dim = Number(dim);
+    } while (isNaN(dim) || dim < min || dim > max)
+    container.innerHTML = '';
+    generateGrid(dim);
 }
 
+function generateGrid (dimension = 16) {
+    for (let i = 0; i < dimension**2; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "squared");
+        square.style.width = `${960/dimension}px`;
+        square.style.height = `${960/dimension}px`;
+        square.addEventListener("mouseover", (e) => e.target.classList.add('hover'));
+        container.appendChild(square);
+    }
+
+}
+
+const container = document.querySelector("#container");
+generateGrid();
+const btn = document.createElement("button");
+btn.textContent = "Generate new grid";
+btn.addEventListener("click", () => askForDimensions())
+document.body.insertBefore(btn, container);
