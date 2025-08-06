09fd83d7bb54347949520bc33da9a40ec4994d2a
HamSudo <192023912+HamSudo@users.noreply.github.com>
2025-08-07 03:16:10 +0400

    Add a darkening effect to the squares: the color darkens by 10% (max is 100%) with every interaction

--- main.js
+++ main.js
@@ -14,10 +14,15 @@ function askForDimensions() {
 function generateGrid (dimension = 16) {
     for (let i = 0; i < dimension**2; i++) {
         const square = document.createElement("div");
-        square.setAttribute("class", "squared");
+        // square.setAttribute("class", "squared");
+        square.setAttribute("data-opacity", '0');
         square.style.width = `${960/dimension}px`;
         square.style.height = `${960/dimension}px`;
-        square.addEventListener("mouseover", (e) => e.target.style.backgroundColor = getRandomRGB());
+        square.addEventListener("mouseover", (e) => {
+            const opacity = increaseOpacity(e);
+            e.target.style.background = `rgba(${getRandomRGB()}, ${opacity})`;
+
+        })
         container.appendChild(square);
     }
 
@@ -27,7 +32,14 @@ function getRandomRGB () {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+    return `${r}, ${g}, ${b}`;
+}
+
+function increaseOpacity (e) {
+    let currentOpacity = parseFloat(e.target.dataset.opacity);
+    currentOpacity = Math.min(currentOpacity + 0.1, 1);
+    e.target.dataset.opacity = currentOpacity.toString();
+    return currentOpacity;
 }
 
 const container = document.querySelector("#container");
