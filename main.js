7fb017d4add8b70f1feccc7f52d873f499c04e04
HamSudo <192023912+HamSudo@users.noreply.github.com>
2025-08-06 11:31:50 +0400

    Set up a hover affect on square boxes: When mouse enters the box its color changes to blue

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++){
         const inner = document.createElement('div');
         inner.setAttribute("class", "inner");
+        inner.addEventListener("mouseover", (e) => e.target.classList.add('hover'));
         outer.appendChild(inner);
     }
     outer.setAttribute("class", "outer");
     container.appendChild(outer);
 }
+
